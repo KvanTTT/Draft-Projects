82df90136338f63c0198796302bc935922389760
Ivan Kochurkin <kvanttt@gmail.com>
2018-04-08 15:53:24 +0300

    Added CSharp6FeatureWalker.

---
+++ Files/CSharp6FeatureWalker.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ConsoleApplication1
+{
+    public class CSharp6FeatureWalker : CSharpSyntaxWalker
+    {
+        public bool CSharp6Featuries { get; private set; }
+
+        public CSharp6FeatureWalker()
+        {
+        }
+
+        public override void VisitPropertyDeclaration(PropertyDeclarationSyntax node)
+        {
+            if (node.ExpressionBody != null)
+            {
+                CSharp6Featuries = true;
+            }
+            else if (node.Initializer != null)
+            {
+                CSharp6Featuries = true;
+            }
+            base.VisitPropertyDeclaration(node);
+        }
+
+        public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            if (node.ExpressionBody != null)
+            {
+                CSharp6Featuries = true;
+            }
+            base.VisitMethodDeclaration(node);
+        }
+
+        public override void VisitOperatorDeclaration(OperatorDeclarationSyntax node)
+        {
+            if (node.ExpressionBody != null)
+            {
+                CSharp6Featuries = true;
+            }
+            base.VisitOperatorDeclaration(node);
+        }
+
+        public override void VisitConversionOperatorDeclaration(ConversionOperatorDeclarationSyntax node)
+        {
+            if (node.ExpressionBody != null)
+            {
+                CSharp6Featuries = true;
+            }
+            base.VisitConversionOperatorDeclaration(node);
+        }
+
+        public override void VisitIndexerDeclaration(IndexerDeclarationSyntax node)
+        {
+            if (node.ExpressionBody != null)
+            {
+                CSharp6Featuries = true;
+            }
+            base.VisitIndexerDeclaration(node);
+        }
+
+        public override void VisitConditionalAccessExpression(ConditionalAccessExpressionSyntax node)
+        {
+            CSharp6Featuries = true;
+            base.VisitConditionalAccessExpression(node);
+        }
+
+        public override void VisitInterpolatedStringExpression(InterpolatedStringExpressionSyntax node)
+        {
+            CSharp6Featuries = true;
+            base.VisitInterpolatedStringExpression(node);
+        }
+
+        public override void VisitCatchFilterClause(CatchFilterClauseSyntax node)
+        {
+            CSharp6Featuries = true;
+            base.VisitCatchFilterClause(node);
+        }
+    }
+}
