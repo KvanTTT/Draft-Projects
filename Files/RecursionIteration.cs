0500b10edd77dc60a4add97cdc6eeb8ccfec96b9
Ivan Kochurkin <kvanttt@gmail.com>
2018-08-18 15:46:24 +0300

    Added RecursionIteration and RecursionIterationTests files.

---
+++ Files/RecursionIteration.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace RecursionIteration
+{
+    public class Samples
+	{
+		public static ulong FactorialRec(ulong n)
+		{
+			return n == 0 ? 1 : FactorialRec(n - 1) * n;
+		}
+
+		public static ulong FactorialRecTail(ulong n)
+		{
+			return n == 0 ? 1 : n * FactorialRecTail(n - 1);
+		}
+
+		public static ulong FactorialIter(ulong n)
+		{
+			var stack = new List<ulong>();
+
+			var k = n;
+			while (k != 0)
+			{
+				stack.Add(k);
+				k = k - 1;
+			}
+			stack.Add(1);
+
+			var result = stack[stack.Count - 1];
+			for (int i = stack.Count - 2; i >= 0; i--)
+				result = result * stack[i];
+
+			return result;
+		}
+
+		public static ulong FactorialIterOpt(ulong n)
+		{
+			ulong result = 1;
+			for (ulong i = 1; i <= n; i++)
+				result = result * i;
+			return result;
+		}
+
+		public static int FibonacciRecursive(int n)
+		{
+			if (n == 0) return 0;
+			if (n == 1) return 1;
+
+			return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+		}
+	}
+}
---
+++ Files/RecursionIterationTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RecursionIteration;
+using System.Diagnostics;
+
+namespace RecursionIteration.Tests
+{
+	[TestFixture]
+	public class FactorialTests
+	{
+		[Test]
+		public void FactorialTest()
+		{
+			var rec = Samples.FactorialRec(0);
+			var iter = Samples.FactorialIter(0);
+			Assert.AreEqual(rec, iter);
+
+			rec = Samples.FactorialRec(1);
+			iter = Samples.FactorialIter(1);
+			Assert.AreEqual(rec, iter);
+
+			rec = Samples.FactorialRec(2);
+			iter = Samples.FactorialIter(2);
+			Assert.AreEqual(rec, iter);
+
+			rec = Samples.FactorialRec(3);
+			iter = Samples.FactorialIter(3);
+			Assert.AreEqual(rec, iter);
+		}
+	}
+}
